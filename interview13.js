da010818feb01a9219f81e356104c50af78c7c08
Jordan Parker <parkerj4321@gmail.com>
2018-11-27 10:00:37 -0600

    Not finished, but I got the concept

---
+++ interview13.js
@@ -0,0 +1,12 @@
+var str = "31@$0";
+var arr = [];
+
+arr = str.split("");
+console.log(arr);
+
+for(var i =0; i<arr.length; i++);{
+    if(arr[0]='3'){
+        arr[0] = 'E';
+    }
+}
+console.log(arr[0]);
